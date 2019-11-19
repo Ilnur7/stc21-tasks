cf4996cbebe70f072947c0f2baafd60113defbf7
Ilnur7 <38868601+Ilnur7@users.noreply.github.com>
2019-11-19 10:37:56 +0300

    Update Test.java

--- src/main/java/part1/lesson08/task01/prod/Test.java
+++ src/main/java/part1/lesson08/task01/prod/Test.java
@@ -8,23 +8,23 @@ public class Test {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите название файла для чтения:");
-        //String nameFileForRead = scanner.nextLine().trim() + ".txt";
-        String nameFileForRead = "data.txt";
+        String nameFileForRead = scanner.nextLine().trim() + ".txt";
+        //String nameFileForRead = "data.txt";
 
         System.out.println("Введите название файла для записи:");
-        //String nameFileForWrite = scanner.nextLine().trim() + ".txt";
-        String nameFileForWrite = "write.txt";
+        String nameFileForWrite = scanner.nextLine().trim() + ".txt";
+        //String nameFileForWrite = "write.txt";
 
         System.out.println("Введите количество итераций:");
-        //int countIteration = Integer.parseInt(scanner.nextLine().trim());
-        int countIteration = 500000;
+        int countIteration = Integer.parseInt(scanner.nextLine().trim());
+        //int countIteration = 500000;
         scanner.close();
 
         Life life = new Life();
         LifeThreads lifeThreads = new LifeThreads();
 
         long start = System.currentTimeMillis();
-        //life.start(nameFileForRead, nameFileForWrite, countIteration);
+        life.start(nameFileForRead, nameFileForWrite, countIteration);
         System.out.println(System.currentTimeMillis() - start);
 
         long startThreads = System.currentTimeMillis();
